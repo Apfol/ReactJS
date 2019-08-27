05df4576cb9a0e57d248ae22c71e80c159425d74
Andrés Fabián Ramos <andresram@unisabana.edu.co>
2019-08-26 21:38:20 -0500

    Agregadas constantes

--- weather-app/src/components/WeatherData.js
+++ weather-app/src/components/WeatherData.js
@@ -1,17 +1,23 @@
 import React from 'react';
 import WeatherExtraInfo from './WeatherExtraInfo';
 import WeatherTemperature from './WeatherTemperature';
-
-const icons = {
-    sunny: "day-sunny",
-    gog: "day-fog",
-};
+import {
+    CLOUD,
+    CLOUDY,
+    SUN,
+    RAIN,
+    SNOW,
+    WINDY,
+} from './../constants/weathers';
 
 const WeatherData = () => {
     return (
         <div>
-            <WeatherTemperature temperature={20} weatherState={''}/>
-            <WeatherExtraInfo humidity={80} wind={"10 m/s"}/>
+            <WeatherTemperature
+                temperature={20}
+                weatherState={SUN}
+            />
+            <WeatherExtraInfo humidity={80} wind={"10 m/s"} />
         </div>
     );
 };
