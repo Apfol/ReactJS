0abfdcfbd19d7ba5d29c4457c1cf485404e4321d
Andrés Fabián Ramos <andresram@unisabana.edu.co>
2019-09-03 18:31:09 -0500

    Added project

--- weather-app/src/App.js
+++ weather-app/src/App.js
@@ -1,13 +1,25 @@
-import React from 'react';
 import WeatherLocation from './components/WeatherLocation';
+import Radium from 'radium';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <WeatherLocation/>
-    </div>
-  );
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    const styles = {
+      width: '60%',
+      height: '100px',
+      border: "1px solid black",
+      '@media (min-width: 500px)': {
+        width: '450px'
+      }
+    }
+    return (
+      <div>
+        <h1 style={styles}>Hello world!</h1>
+      </div>
+    );
+  }
 }
 
-export default App;
+export default Radium(App);
