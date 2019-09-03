492a22fbc4f9b2ff7700c8b437767da94ef74ac6
Andrés Fabián Ramos <andresram@unisabana.edu.co>
2019-09-03 18:47:51 -0500

    Agregado Modules

--- style-css/src/App.js
+++ style-css/src/App.js
@@ -1,24 +1,16 @@
 import Radium from 'radium';
-import './App.css';
 
 import React, { Component } from 'react';
+import { classes } from './App.css';
 
 class App extends Component {
   render() {
-    const styles = {
-      width: '60%',
-      height: '100px',
-      border: "1px solid black",
-      '@media (min-width: 500px)': {
-        width: '450px'
-      }
-    }
     return (
       <div>
-        <h1 style={styles}>Hello world!</h1>
+        <h1 className={classes.heading}>Hello world</h1>
       </div>
     );
   }
 }
 
-export default Radium(App);
+export default App;
